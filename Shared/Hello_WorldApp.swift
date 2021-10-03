5d3bd5a7f34e4e75b9527a32b3ff15cd467ad133
Nina <nnotomi@gmail.com>
2021-10-04 03:55:37 +1100

    New project

---
+++ Shared/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Shared
+//
+//  Created by Nina on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
